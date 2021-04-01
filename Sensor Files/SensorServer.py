21d820f6b1bafc80cf360429ff114f03b1f1aeef
Vanshsmann <61492182+Vanshsmann@users.noreply.github.com>
2021-04-01 17:56:29 -0500

    Updated code with UTD Info

--- Sensor Files/SensorServer.py
+++ Sensor Files/SensorServer.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
-from sense_emu import SenseHat
+##from sense_emu import SenseHat
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    sense = SenseHat()
+   ## sense = SenseHat()
 
-    celcius = round(sense.get_temperature(), 1)
-    fahrenheit = round(1.8 * celcius + 32, 1)
-    humidity = round(sense.get_humidity(), 1)
-    pressure = round(sense.get_pressure(), 1)
+    celcius = 32 ##round(sense.get_temperature(), 1)
+    fahrenheit = 68 ##round(1.8 * celcius + 32, 1)
+    humidity = 54.2##round(sense.get_humidity(), 1)
+    pressure = 789##round(sense.get_pressure(), 1)
 
     return render_template('SensorSite.html', celcius=celcius, fahrenheit=fahrenheit, humidity=humidity, pressure=pressure)
 

00fd83d264e0840baa45e3e98137d5e77f4520b7
Vanshsmann <61492182+Vanshsmann@users.noreply.github.com>
2021-04-12 21:47:07 -0500

    Removed Live Graph for now and added graph

--- MainWebHost.py
+++ MainWebHost.py
@@ -3,7 +3,7 @@ import json
 import time
 
 from flask import Flask, Response, render_template
-fname = " "
+
 application = Flask(__name__)
 #Check how many beehives there are
 def addDate(YMD, time):
@@ -22,10 +22,26 @@ def LastNlines(fname, N):
 
             return x
 
-def createvariables
+
+def collectallfunction(filenameforbeehive):
+    datapoint = "["
+    with open(filenameforbeehive) as file:
+        for readline in file:
+            # For Python3, use print(line)
+            readlist = readline.split()
+            datapoint = datapoint + "('" + readlist[0] + " " + readlist[1] + "'," + readlist[2] + ")" +  ", "
+        datapoint = datapoint[:-2]
+        datapoint = datapoint + "]"
+    return datapoint
 
 
 
+
+    #Grabs the date and temp
+    # stores date and temp in this format ('date', temp)
+    #does this for whole file
+    #put into list and append
+    #return list
 
 
 @application.route('/') # This displays the main parts of the website
@@ -38,21 +54,29 @@ def index():
     fahrenheit = float(list[2])
     humidity = list[6]
     weight = list[12]
-    return render_template('UTDBeeSite.html', fahrenheit=fahrenheit, humidity=humidity, weight=weight)
+
+    return render_template('UTDBeeSite.html', fahrenheit=fahrenheit, humidity=humidity, weight=weight, )
 
 
 @application.route('/<UTDBeehive>')
 def UTDBeehive(UTDBeehive):
     fname = UTDBeehive + ".txt"
     list = LastNlines(fname, 1)
-    print(list)
+    #print(list)
     date = addDate(list[0], list[1])
-    print(date)
+    #print(date)
     fahrenheit = float(list[2])
     humidity = list[6]
     weight = list[12]
-    return render_template('UTDHiveSite.html', fahrenheit=fahrenheit, humidity=humidity, weight=weight)
-
+    data = [('04-01-2021', 150),('04-02-2021', 140),('04-03-2021', 150)]
+    print(type(data))
+    str = collectallfunction(fname)
+    print(str)
+    #data = str
+    #print(data)
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template('UTDHiveSite.html', fahrenheit=fahrenheit, humidity=humidity, weight=weight, labels=labels,values=values)
 
 
 

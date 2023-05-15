c653a43d3990f9decdcf2418f2c71940638aaf58
Hassan H <118084883+hkacperh@users.noreply.github.com>
2023-05-14 20:30:38 -0500

    Update index.js
    Updated index.js to latest version

--- server/index.js
+++ server/index.js
@@ -1,93 +1,141 @@
+// Last edited May 7 2023
+
+
+// Boilerplate code start
 const express = require("express");
 const pool = require("./db");
 const path = require("path");
-var cors = require('cors');
+var cors = require("cors");
 
 const app = express();
 
+
+/*
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
+
 app.use(
-    cors({
-      origin: "http://localhost:3000",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true
-    })
-  )
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+*/
 
-
+app.use(cors(
+{
+  origin: "http://10.159.186.209:3000", // rebecca's machine IP address
+  optionsSuccessStatus: 200
+}));
 
 app.use(express.json());
 
+// Boilerplate code end
 
+const allData = [];
 
-const allData= [];
-
-app.get("/data", (req, res) =>{
-    return res.json(allData);
-})
-
-app.post("/data", (req, res) =>{
-    const value = req.body;
-
-    const data = {
-        value
-    }
-
-    allData.push(value.x);
-    return res.json(value)
-})
-
-
-app.get("/databaseData", async (req, res) => {
-    try {
-        const allTodos = await pool.query("SELECT * FROM weightTable");
-        res.json(allTodos.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
+// Get request to ESP32
+app.get("/data", (req, res) => {
+  console.log("Hello from /data API");
+  return res.json(allData);
 });
 
+// Don't really need this because we already have a GET request for data from the ESP32
+app.post("/data", (req, res) => {
+  const value = req.body;
+
+  const data = {
+    value,
+  };
+
+  allData.push(value.x);
+  return res.json(value);
+});
+
+
+// GET request from website to send weight data
+app.get("/databaseDataWeight", async (req, res) => {
+  try {
+    console.log("\n /databaseDataWeight GET Request called!");
+    const allTodos = await pool.query("SELECT * FROM weightTable");
+    // console.log(allTodos.rows[0]);
+    return res.json({ "weightData": allTodos.rows });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// GET request from website to send temp data
+app.get("/databaseDataTemp", async (req, res) => {
+  try {
+    console.log("\n /databaseDataTemp GET Request called!");
+    const allTodos = await pool.query("SELECT * FROM temperatureTable");
+    // console.log(allTodos.rows[0]);
+    return res.json({ "tempData": allTodos.rows });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// GET request from website to send humidity data
+app.get("/databaseDataHumidity", async (req, res) => {
+  try {
+    console.log("\n /databaseDataHumidity GET Request called!");
+    const allTodos = await pool.query("SELECT * FROM humidityTable");
+    // console.log(allTodos.rows[0]);
+    return res.json({ "humidityData": allTodos.rows });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+
+
+// POST request to put data into database
 app.post("/databaseData", async (req, res) => {
-    try {
-        const { x } = req.body;
-        const newValue = await pool.query(
-            "INSERT INTO weightTable (value) VALUES($1) RETURNING *",
-            [x]
-        );
+  try {
+    console.log("/databaseData Called()");
+    const { weight } = req.body;
+    console.log("{ w, t, h }", weight, "72.818250", "48.859320");
+    const newValue1 = await pool.query(
+      "INSERT INTO weightTable (testdata) VALUES($1) RETURNING *",
+      [weight]
+    );
 
-        res.json(newValue.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+
+    res.json({"weight": newValue1.rows[0]});
+  } catch (err) {
+    console.error(err.message);
+  }
 });
-
 
 app.delete("/databaseData/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM weightTable WHERE weightid = $1", [
-      id
-    ]);
+    const deleteTodo = await pool.query(
+      "DELETE FROM weightTable WHERE weightid = $1",
+      [id]
+    );
     res.json("value deleted!");
   } catch (err) {
     console.log(err.message);
   }
 });
 
+app.use(express.static(path.join(__dirname, "/views")));
 
-app.use(express.static(path.join(__dirname, '/views')));
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/views', 'index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views", "index.html"));
 });
 
-
-app.listen(3000, () => console.log("http://localhost:3000"));
-
+app.listen(5008, () =>
+  console.log("Server running on PORT: 5008 @ http://localhost:5008")
+);
